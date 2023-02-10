2f097d64537438350f83e3883cc38f929a7fd920
Tim <timtran007@gmail.com>
2023-02-10 00:43:13 -0600

    update routes for all controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :following_ships
+  resources :ingredients
+  resources :recipes
+  resources :shopping_lists
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
