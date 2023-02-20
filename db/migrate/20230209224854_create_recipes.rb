aaa183716c958adcd7e34218ca8d03b8404dfa0b
Tim <timtran007@gmail.com>
2023-02-20 16:40:37 -0600

    adds association to create Recipes migration + image url and likes

--- db/migrate/20230209224854_create_recipes.rb
+++ db/migrate/20230209224854_create_recipes.rb
@@ -2,10 +2,12 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
       t.string :title
+      t.string :image_url
       t.text :instructions
       t.integer :cook_time_in_minutes
       t.integer :prep_time_in_minutes
-      t.belongs_to :User, null: false, foreign_key: true
+      t.integer :likes
+      t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
     end
