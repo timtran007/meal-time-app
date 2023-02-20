bd64137adc702fd5596faafa4ba4ebd60638a289
Tim <timtran007@gmail.com>
2023-02-20 17:12:24 -0600

    adds rescue from RecordInvalid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+    private
+
+    def render_unprocessable_entity(invalid)
+        render json: {error: invalid.record.errors.full_messages}, status: 422
+    end
 end
