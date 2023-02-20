c700f2878c1033a6975704a36250a2abd8a90d1a
Tim <timtran007@gmail.com>
2023-02-20 13:10:54 -0600

    add follower and following feature for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
     has_many :recipes
     has_many :shopping_lists
-    has_many :following_ships
+    has_many :following_ships, foreign_key: :user_1
     has_many :followings, through: :following_ships, foreign_key: :user_1, source: :user_2
+
+    has_many :follower_ship, foreign_key: :user_2, class_name: "FollowingShip"
+    has_many :followers, through: :follower_ship, foreign_key: :user_2, source: :user_1
 end
+ 
