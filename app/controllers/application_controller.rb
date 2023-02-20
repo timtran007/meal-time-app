737cde452317113be44a35174c2f566a6b872bfd
Tim <timtran007@gmail.com>
2023-02-20 17:14:35 -0600

    adds rescue from RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
+    rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+    
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
     private
+
+    def render_record_not_found
+        render json: {error: ["record not found"]}, status: :not_found
+    end
 
     def render_unprocessable_entity(invalid)
         render json: {error: invalid.record.errors.full_messages}, status: 422
