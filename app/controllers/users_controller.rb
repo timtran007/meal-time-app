942c08adac7cc66ef1ed102579f839741bf54de3
Tim <timtran007@gmail.com>
2023-02-20 17:16:48 -0600

    adds create action for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+    
+    def create
+        user = User.create!(user_params)
+        if user &.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}
+        end
+    end
 end
