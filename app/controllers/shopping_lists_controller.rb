1b0250a6cab988f44233e6a3aee532fc729d9156
Tim <timtran007@gmail.com>
2023-02-21 00:46:55 -0600

    adds show, create, update and destroy actions for shopping_list

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,2 +1,37 @@
 class ShoppingListsController < ApplicationController
+
+    def show
+        find_user
+        shopping_list = find_user.shopping_lists.find(params[:id])
+        render json: shopping_list
+    end
+
+    def create
+        find_user
+        shopping_list = find_user.shopping_lists.create!(shopping_list_params)
+        render json: shopping_list, status: :created
+    end
+
+    def update
+        find_user
+        shopping_list = find_user.shopping_lists.find(params[:id])
+        shopping_list.update(shopping_list_params)
+        render json: shopping_list
+    end
+
+    def destroy
+        find_user
+        shopping_list = find_user.shopping_lists.find(params[:id])
+        shopping_list.destroy
+        render json: shopping_list
+    end
+
+
+    private
+
+    def shopping_list_params
+        params.permit(:name, :date, :user_id)
+    end
+
+
 end
