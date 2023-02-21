85497a687e85005356dcfbdb5411d8dc076d96c7
Tim <timtran007@gmail.com>
2023-02-20 21:42:24 -0600

    sets up index and create actions and framework for create, update and destroy

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,20 @@
 class RecipesController < ApplicationController
+    def index
+        recipes = Recipe.all
+        render json: recipes
+    end
+
+    def show
+        recipe = Recipe.find(params[:id])
+        render json: recipe
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
