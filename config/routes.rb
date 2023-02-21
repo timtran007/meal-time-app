d8233cbe4e565d424571e7de915690050ea60856
Tim <timtran007@gmail.com>
2023-02-20 20:55:18 -0600

    updates login, logout and auto login routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   resources :recipes
   resources :shopping_lists
   
-  post 'signup', to: 'users#create'
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
