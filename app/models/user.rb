ab0f4359e7e4444c3ff495fe34dbebc816534e2b
Tim <timtran007@gmail.com>
2023-02-21 11:18:36 -0600

    adds has_secure_password to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+
     has_many :recipes
     has_many :shopping_lists
     has_many :following_ships, foreign_key: :user_1
@@ -6,5 +8,6 @@ class User < ApplicationRecord
 
     has_many :follower_ship, foreign_key: :user_2, class_name: "FollowingShip"
     has_many :followers, through: :follower_ship, foreign_key: :user_2, source: :user_1
+
 end
  
