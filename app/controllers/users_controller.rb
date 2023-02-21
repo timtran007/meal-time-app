a913f82bb24fdaca00b89942109b7740787fdfae
Tim <timtran007@gmail.com>
2023-02-20 21:16:06 -0600

    adds private method user_params as a strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,10 @@ class UsersController < ApplicationController
             render json: {errors: user.errors.full_messages}
         end
     end
+    
+    private
+
+    def user_params
+        params.permit(:name, :email, :password, :password_confirmation, :image_url)
+    end
 end
