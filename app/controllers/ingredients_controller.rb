464bca9a9d498fed198e0c4b388406a4dd126f86
Tim <timtran007@gmail.com>
2023-02-21 09:57:22 -0600

    adds index and show actions

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,11 @@
 class IngredientsController < ApplicationController
+    def index
+        ingredients = Ingredient.all
+        render json: ingredients
+    end
+
+    def show
+        ingredient = Ingredient.find(params[:id])
+        render json: ingredient
+    end
 end
