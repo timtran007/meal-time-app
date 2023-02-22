d0136580f2dd1adcb3d0b8d491efc9c3255b8ccf
Tim <timtran007@gmail.com>
2023-02-22 01:22:55 -0600

    creates index create and show_following_recipes actions

--- app/controllers/following_ships_controller.rb
+++ app/controllers/following_ships_controller.rb
@@ -1,2 +1,19 @@
 class FollowingShipsController < ApplicationController
+
+    def index
+        following_ships = FollowingShip.all
+        render json: following_ships
+    end
+    def create
+        following = FollowingShip.create!(user_1_id: session[:user_id], user_2_id: params[:user_2_id])
+        render json: following
+    end
+
+    def show_following_recipes
+        find_user
+        following = find_user.followings
+        render json: following
+    end
+
+
 end
