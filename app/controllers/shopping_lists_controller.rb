7761f5a98bdd940d4d4d06e7ed9e1a6016175bcd
Tim <timtran007@gmail.com>
2023-02-22 02:33:40 -0600

    adds ability to create ingredient inside of shopping_list

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,4 +1,11 @@
 class ShoppingListsController < ApplicationController
+    before_action :authorize
+    
+    def index
+        find_user
+        shopping_lists = find_user.shopping_lists
+        render json: shopping_lists
+    end
 
     def show
         find_user
@@ -26,6 +33,14 @@ class ShoppingListsController < ApplicationController
         render json: shopping_list
     end
 
+    #creating ingredient inside of a shopping_list
+    def create_ingredient
+        find_user
+        shopping_list = find_user.shopping_lists.find(params[:id])
+        ingredient = shopping_list.ingredients.create!(ingredient_params)
+        render json: ingredient, status: :created
+    end
+
 
     private
 
@@ -33,5 +48,8 @@ class ShoppingListsController < ApplicationController
         params.permit(:name, :date, :user_id)
     end
 
+    def ingredient_params
+        params.permit(:name, :category, :shopping_list_id, :quantity, :measurement)
+    end
 
 end
