51dbec2efcd0d3496dcbc551f4c67f84abd2e166
Tim <timtran007@gmail.com>
2023-02-22 12:02:00 -0600

    fixes naming convention

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
     has_many :following_ships, foreign_key: :user_1
     has_many :followings, through: :following_ships, foreign_key: :user_1, source: :user_2
 
-    has_many :follower_ship, foreign_key: :user_2, class_name: "FollowingShip"
-    has_many :followers, through: :follower_ship, foreign_key: :user_2, source: :user_1
+    has_many :follower_ships, foreign_key: :user_2, class_name: "FollowingShip"
+    has_many :followers, through: :follower_ships, foreign_key: :user_2, source: :user_1
 
 end
  
