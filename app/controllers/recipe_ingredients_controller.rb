50dab70439133f5beac5882e035b960fbdd159d2
Tim <timtran007@gmail.com>
2023-02-22 23:33:12 -0600

    adds create, update and destroy to recipe ingredients controller

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -1,2 +1,26 @@
 class RecipeIngredientsController < ApplicationController
+    
+    def create
+        recipe = Recipe.find(params[:recipe_id])
+        ingredient = recipe.recipe_ingredients.create!(recipe_ingredient_params)
+        render json: ingredient, status: :created
+    end
+
+    def update
+        recipe = Recipe.find(params[:recipe_id])
+        ingredient = recipe.recipe_ingredients.update(recipe_ingredient_params)
+        render json: ingredient, status: :created
+    end
+
+    def destroy
+        ingredient = RecipeIngredient.find(params[:id])
+        ingredient.destroy
+        render json: ingredient
+    end
+
+    private
+    def recipe_ingredient_params
+        params.permit(:name, :category, :shopping_list_id, :quantity, :measurement)
+    end
+
 end
