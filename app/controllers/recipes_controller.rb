2dc10e158387011e789f67c390a7a285b2b1e19a
Tim <timtran007@gmail.com>
2023-02-22 23:32:46 -0600

    adds before action authorize and skip_before_action, find id by :recipe_id

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,8 @@
 class RecipesController < ApplicationController
+    before_action :authorize
+
+    skip_before_action :authorize, only: [:index, :show]
+
     def index
         recipes = Recipe.all
         render json: recipes
@@ -29,12 +33,10 @@ class RecipesController < ApplicationController
         render json: recipe
     end
 
-    #add recipe ingredients action
-
     #add follow_user action /recipe/:id, to "recipes#follow_user"
     def follow_user
         find_user
-        recipe = Recipe.find(params[:id])
+        recipe = Recipe.find(params[:recipe_id])
         user = recipe.user
         followship = find_user.following_ships.create!(user_1_id: session[:user_id], user_2_id: user.id)
         render json: followship
