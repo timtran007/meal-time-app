a80ec2868fabce2a0edd2fe4916436df339e5479
Tim <timtran007@gmail.com>
2023-02-22 23:33:49 -0600

    updates routes.rb file to reflect changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
 Rails.application.routes.draw do
-  resources :recipe_ingredients
-  #run rails g resource for recipe_ingredients
+
   resources :following_ships
-  resources :ingredients, only: [:index, :show]
   resources :recipes
   resources :shopping_lists
+  resources :recipe_ingredients, only: [:create, :update, :destroy]
+  resources :ingredients, only: [:create, :update, :destroy]
+
 
   get '/users', to: 'users#index'
-
-  get '/following-recipes', to: 'following_ships#show_following_recipes'
   get '/user-recipes', to: 'users#user_recipes'
-  post '/shopping_lists/:id', to: 'shopping_lists#create_ingredient'
-  post '/recipes/:id', to: 'recipes#follow_user'
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
+
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
+
+  get '/following-recipes', to: 'following_ships#show_following_recipes'
+
+  post '/follow-user-recipes/', to: 'recipes#follow_user'
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
