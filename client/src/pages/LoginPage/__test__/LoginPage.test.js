821acafabae4ec5861c4c98ec5cf6fd31a78a1bf
Tim <timtran007@gmail.com>
2023-02-26 14:09:31 -0600

    fixes login page test

--- client/src/pages/LoginPage/__test__/LoginPage.test.js
+++ client/src/pages/LoginPage/__test__/LoginPage.test.js
@@ -1,19 +1,28 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import LoginPage from '../LoginPage'
 
+const MockLogin = () => {
+  return(
+    <BrowserRouter>
+      <LoginPage />
+    </BrowserRouter>
+  )
+}
+
 it('renders the login page successfully', () => {
-  render(<LoginPage />);
-  const heading = screen.getByRole('heading', {name: /login/i});
+  render(<MockLogin />);
+  const heading = screen.getByRole('heading', {name: /log into your account/i});
   expect(heading).toBeInTheDocument();
 });
 
 it('renders the LoginForm component', () => {
-    render(<LoginPage />);
-    const form = screen.getByRole('form', {name: /login form/i})
+    render(<MockLogin />);
+    const form = screen.getByRole('button', {name: /login/i})
   })
 
 it("renders the text don't have an account yet sign up here", () => {
-    render(<LoginPage />);
+    render(<MockLogin />);
     const text = screen.getByText(/don't have an account yet?/i)
     expect(text).toBeInTheDocument();
 })
