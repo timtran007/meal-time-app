ce800b7fcfd10c291c63b115406f90efb662966d
Tim <timtran007@gmail.com>
2023-02-26 12:06:13 -0600

    makes updates to new shopping list form test

--- client/src/components/Forms/NewShoppingListForm/__test__/NewShoppingListForm.test.js
+++ client/src/components/Forms/NewShoppingListForm/__test__/NewShoppingListForm.test.js
@@ -5,7 +5,7 @@ describe('render new shopping list form', () => {
 
     it('renders name input', () => {
         render(<NewShoppingListForm />);
-        const nameInput = screen.getByLabelText({ name: /name/i })
+        const nameInput = screen.getByLabelText(/name/i)
         expect(nameInput).toBeInTheDocument()
     })
     
@@ -14,20 +14,20 @@ describe('render new shopping list form', () => {
 describe('new shopping lists form functionality', () => {
     it('should be able to type into name input', () => {
         render(<NewShoppingListForm />);
-        const nameInput = screen.getByLabelText({ name: /name/i })
+        const nameInput = screen.getByLabelText(/name/i)
         fireEvent.change(nameInput, { target: { value: "Monday's Grocery List"} })
-        expect(nameInput).toBe("ribeye steak")
+        expect(nameInput.value).toBe("ribeye steak")
     })
     
     //clears out the the input fields once button is clicked
 
     it('should clear name input after button click', () => {
         render(<NewShoppingListForm />);
-        const nameInput = screen.getByLabelText({ name: /name/i })
+        const nameInput = screen.getByLabelText(/name/i)
         const button = screen.getByRole( 'button', { name: /add ingredient/i})
         fireEvent.change(nameInput, { target: { value: "Monday's Grocery List"} })
         fireEvent.click(button)
-        expect(nameInput).toBe("")
+        expect(nameInput.value).toBe("")
     })
     
 })
