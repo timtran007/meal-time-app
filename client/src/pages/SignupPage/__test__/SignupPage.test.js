de741689b2fac254cacea6950312d32f06be188d
Tim <timtran007@gmail.com>
2023-02-26 13:40:26 -0600

    wraps browser router around component

--- client/src/pages/SignupPage/__test__/SignupPage.test.js
+++ client/src/pages/SignupPage/__test__/SignupPage.test.js
@@ -1,20 +1,30 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import SignupPage from '../SignupPage'
 
+const MockSignUp = () => {
+  return(
+  <BrowserRouter>
+    <SignupPage />
+  </BrowserRouter>
+  )
+}
+
+
 it('renders the Signup page successfully', () => {
-  render(<SignupPage />);
+  render(<MockSignUp />);
   const heading = screen.getByRole('heading', {name: /sign up/i});
   expect(heading).toBeInTheDocument();
 });
 
 it('renders the signup page form successfully', () => {
-    render(<SignupPage />);
-    const form = screen.getByRole('form', {name: /sign up/i});
-    expect(form).toBeInTheDocument();
+    render(<MockSignUp />);
+    const button = screen.getByRole('button', {name: /sign up/i});
+    expect(button).toBeInTheDocument();
   });
 
   it("renders the text have an account already?", () => {
-    render(<SignupPage />);
-    const text = screen.getByText(/have an account already?/i)
+    render(<MockSignUp />);
+    const text = screen.getByText(/have an account/i)
     expect(text).toBeInTheDocument();
 })
