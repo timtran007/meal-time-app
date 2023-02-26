568afbb311c21396b2b11c073d9fb083e055ef57
Tim <timtran007@gmail.com>
2023-02-26 12:03:16 -0600

    fixes validations for measurement within RecipeIngredient model

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -3,5 +3,5 @@ class RecipeIngredient < ApplicationRecord
 
   validates :name, :quantity, presence: true
   validates :category, inclusion: { in: %w(meats produce dairy spices baking drinks grains canned_goods), message: "%{value} is not a valid category"}
-  validates :measurement, inclusion: { in: %w(cup cups ounce ounces tablespoon tablespoons teaspoon teaspoons quart quarts), message: "%{value} is not a valid measurement"}
+  validates :measurement, inclusion: { in: %w(cup/cups ounce/ounces tablespoon/tablespoons teaspoon/teaspoons quart/quarts), message: "%{value} is not a valid measurement"}
 end
