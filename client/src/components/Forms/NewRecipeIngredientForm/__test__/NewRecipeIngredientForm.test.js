ad28741fe1fa654d25ab0a2a4fe0d6e6acba1335
Tim <timtran007@gmail.com>
2023-02-26 11:41:43 -0600

    fixes bugs in new recipe ingredient form test

--- client/src/components/Forms/NewRecipeIngredientForm/__test__/NewRecipeIngredientForm.test.js
+++ client/src/components/Forms/NewRecipeIngredientForm/__test__/NewRecipeIngredientForm.test.js
@@ -5,25 +5,25 @@ describe('render new recipe ingredients form', () => {
 
     it('renders name input', () => {
         render(<NewRecipeIngredientForm />);
-        const nameInput = screen.getByLabelText({ name: /name/i })
+        const nameInput = screen.getByPlaceholderText(/name/i)
         expect(nameInput).toBeInTheDocument()
     })
 
     it('renders category input', () => {
         render(<NewRecipeIngredientForm />);
-        const categoryInput = screen.getByLabelText({ name: /category/i })
+        const categoryInput = screen.getByDisplayValue(/select a category/i)
         expect(categoryInput).toBeInTheDocument()
     })
 
     it('renders quantity input', () => {
         render(<NewRecipeIngredientForm />);
-        const quantityInput = screen.getByLabelText({ name: /quantity/i })
+        const quantityInput = screen.getByPlaceholderText(/quantity/i)
         expect(quantityInput).toBeInTheDocument()
     })
 
     it('renders measurement input', () => {
         render(<NewRecipeIngredientForm />);
-        const measurementInput = screen.getByLabelText({ name: /measurement/i })
+        const measurementInput = screen.getByDisplayValue(/select a measurement/i)
         expect(measurementInput).toBeInTheDocument()
     })
     
@@ -32,67 +32,67 @@ describe('render new recipe ingredients form', () => {
 describe('new recipe ingredient form functionality', () => {
     it('should be able to type into name input', () => {
         render(<NewRecipeIngredientForm />);
-        const nameInput = screen.getByLabelText({ name: /name/i })
+        const nameInput = screen.getByPlaceholderText(/name/i)
         fireEvent.change(nameInput, { target: { value: "ribeye steak"} })
-        expect(nameInput).toBe("ribeye steak")
+        expect(nameInput.value).toBe("ribeye steak")
     })
     
     it('should be able to type into category input', () => {
         render(<NewRecipeIngredientForm />);
-        const categoryInput = screen.getByLabelText({ name: /category/i })
+        const categoryInput = screen.getByDisplayValue(/select a category/i)
         fireEvent.change(categoryInput, { target: { value: "meats"} })
-        expect(categoryInput).toBe("meats")
+        expect(categoryInput.value).toBe("meats")
     })
     
     it('should be able to type into quantity input', () => {
         render(<NewRecipeIngredientForm />);
-        const quantityInput = screen.getByLabelText({ name: /quantity/i })
+        const quantityInput = screen.getByPlaceholderText(/quantity/i)
         fireEvent.change(quantityInput, { target: { value: "16"} })
-        expect(quantityInput).toBe("16")
+        expect(quantityInput.value).toBe("16")
     })
     
     it('should be able to type into measurement input', () => {
         render(<NewRecipeIngredientForm />);
-        const measurementInput = screen.getByLabelText({ name: /measurement/i })
+        const measurementInput = screen.getByDisplayValue(/select a measurement/i)
         fireEvent.change(measurementInput, { target: { value: "ounces"} })
-        expect(measurementInput).toBe("ounces")
+        expect(measurementInput.value).toBe("ounces")
     })
 
     //clears out the the input fields once button is clicked
 
     it('should clear name input after button click', () => {
         render(<NewRecipeIngredientForm />);
-        const nameInput = screen.getByLabelText({ name: /name/i })
+        const nameInput = screen.getByPlaceholderText(/name/i)
         const button = screen.getByRole( 'button', { name: /add ingredient/i})
         fireEvent.change(nameInput, { target: { value: "ribeye"} })
         fireEvent.click(button)
-        expect(nameInput).toBe("")
+        expect(nameInput.value).toBe("")
     })
     
     it('should clear category input after button click', () => {
         render(<NewRecipeIngredientForm />);
-        const categoryInput = screen.getByLabelText({ name: /category/i })
+        const categoryInput = screen.getByDisplayValue(/select a category/i)
         const button = screen.getByRole( 'button', { name: /add ingredient/i})
         fireEvent.change(categoryInput, { target: { value: "meats"} })
         fireEvent.click(button)
-        expect(categoryInput).toBe("")
+        expect(categoryInput.value).toBe("Select a category...")
     })
     
     it('should clear quantity input after button click', () => {
         render(<NewRecipeIngredientForm />);
-        const quantityInput = screen.getByLabelText({ name: /quantity/i })
+        const quantityInput = screen.getByPlaceholderText(/quantity/i)
         const button = screen.getByRole( 'button', { name: /add ingredient/i})
         fireEvent.change(quantityInput, { target: { value: "16"} })
         fireEvent.click(button)
-        expect(quantityInput).toBe("")
+        expect(quantityInput.value).toBe("")
     })
     
     it('should clear measurement input after button click', () => {
         render(<NewRecipeIngredientForm />);
-        const measurementInput = screen.getByLabelText({ name: /measurement/i })
+        const measurementInput = screen.getByDisplayValue(/select a measurement/i)
         const button = screen.getByRole( 'button', { name: /add ingredient/i})
         fireEvent.change(measurementInput, { target: { value: "ounces"} })
         fireEvent.click(button)
-        expect(measurementInput).toBe("")
+        expect(measurementInput.value).toBe("Select a measurment...")
     })
 })
