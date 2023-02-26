00d67f064a65e5395e185a7c38f979923a9253a6
Tim <timtran007@gmail.com>
2023-02-26 03:47:47 -0600

    adds hooks from bootstrap and react

---
+++ client/src/components/Forms/LoginForm/LoginForm.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+
+function LoginForm() {
+  return (
+    <div>LoginForm</div>
+  )
+}
+
+export default LoginForm
