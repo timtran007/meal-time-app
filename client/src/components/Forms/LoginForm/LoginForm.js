f7e76bc85afac8ead4565a0805a7e39c9ec7fb73
Tim <timtran007@gmail.com>
2023-02-26 04:10:09 -0600

    adds state, handle change function and Form.Control id to pass first test

--- client/src/components/Forms/LoginForm/LoginForm.js
+++ client/src/components/Forms/LoginForm/LoginForm.js
@@ -5,22 +5,37 @@ import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 
 function LoginForm() {
+
+    const initialFormData = {
+        email: '',
+        password: '',
+    }
+
+    const [formData, setFormData] = useState(initialFormData)
+
+    function handleChange(e) {
+        const key = e.target.name
+        const value = e.target.value
+        setFormData({
+            ...formData,
+            [key]: value
+        })
+    }
+
   return (
     <Form>
-        <Form.Group as={Row} className={formField}>
-            <Form.Label
-                htmlFor='email'
-            >
-                Email:
+        <Form.Group as={Row}>
+            <Form.Label htmlFor='email'>
+                Email
             </Form.Label>
             <Col>
                 <Form.Control
+                    id='email'
                     name='email'
                     type='input'
-                    onChange={() => {}}
-                    value={formData}
+                    onChange={handleChange}
+                    value={formData.email}
                 >
-                    
                 </Form.Control>
             </Col>
         </Form.Group>
