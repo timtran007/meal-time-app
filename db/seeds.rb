e501b799c2b12f217efd3e24eaa178499ddfe72e
Tim <timtran007@gmail.com>
2023-02-27 01:04:07 -0600

    updates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-tim = User.create(name: 'Tim', email: 'tim@gmail.com', password: '12345')
-ted = User.create(name: 'Ted', email: 'ted@gmail.com', password: '12345')
-vanessa = User.create(name: 'Vanessa', email: 'vanessa@gmail.com', password: '12345')
-noah = User.create(name: 'Noah', email: 'noah@gmail.com', password: '12345') 
+tim = User.create(name: 'Tim', email: 'tim@gmail.com', password: '12345', password_confirmation: '12345')
+ted = User.create(name: 'Ted', email: 'ted@gmail.com', password: '12345', password_confirmation: '12345')
+vanessa = User.create(name: 'Vanessa', email: 'vanessa@gmail.com', password: '12345', password_confirmation: '12345')
+noah = User.create(name: 'Noah', email: 'noah@gmail.com', password: '12345', password_confirmation: '12345') 
 
 FollowingShip.create(user_1_id: noah.id, user_2_id: tim.id)
 FollowingShip.create(user_1_id: vanessa.id, user_2_id: tim.id)
@@ -61,24 +61,24 @@ Step 9: serve on warmed up flour or corn tortillas', cook_time_in_minutes: 30, p
 
 monday_night_groceries = ShoppingList.create(name:'Mondays Groceries', user_id: tim.id)
 
-ribeye_16oz = RecipeIngredient.create(name: 'ribeye', category: 'meats', quantity: '16', measurement: 'ounces', recipe_id: ribeye_steak.id)
+ribeye_16oz = RecipeIngredient.create(name: 'ribeye', category: 'meats', quantity: '16', measurement: 'ounce/ounces', recipe_id: ribeye_steak.id)
 
-salt_1_TBSP = RecipeIngredient.create(name: 'salt', category: 'spices', quantity: '1', measurement: 'tablespoon', recipe_id: ribeye_steak.id)
+salt_1_TBSP = RecipeIngredient.create(name: 'salt', category: 'spices', quantity: '1', measurement: 'tablespoon/tablespoons', recipe_id: ribeye_steak.id)
 
-pepper_1_tsp = RecipeIngredient.create(name: 'pepper', category: 'spices', quantity: '1', measurement: 'teaspoon', recipe_id: ribeye_steak.id)
+pepper_1_tsp = RecipeIngredient.create(name: 'pepper', category: 'spices', quantity: '1', measurement: 'teaspoon/teaspoons', recipe_id: ribeye_steak.id)
 
-butter_1_TBSP = RecipeIngredient.create(name: 'butter', category: 'dairy', quantity: '1', measurement: 'tablespoon', recipe_id: ribeye_steak.id)
+butter_1_TBSP = RecipeIngredient.create(name: 'butter', category: 'dairy', quantity: '1', measurement: 'tablespoon/tablespoons', recipe_id: ribeye_steak.id)
 
-lime = RecipeIngredient.create(name: 'lime', category: 'produce', quantity: "3", measurement: 'individual', recipe_id: brazilian_lemonade.id)
+lime = RecipeIngredient.create(name: 'lime', category: 'produce', quantity: "8", measurement: 'ounce/ounces', recipe_id: brazilian_lemonade.id)
 
 condensed_milk_third_cup = RecipeIngredient.create(name: 'condensed milk', category: 'baking', quantity: '1/3', measurement: 'cup', recipe_id: brazilian_lemonade.id)
 
-sugar_third_cup = RecipeIngredient.create(name: 'sugar', category: 'baking', quantity: '1/3', measurement: 'cup', recipe_id: brazilian_lemonade.id)
+sugar_third_cup = RecipeIngredient.create(name: 'sugar', category: 'baking', quantity: '1/3', measurement: 'cup/cups', recipe_id: brazilian_lemonade.id)
 
-water_4_cups = RecipeIngredient.create(name: 'water', category: 'drinks', quantity: '4', measurement: 'cup', recipe_id: brazilian_lemonade.id)
+water_4_cups = RecipeIngredient.create(name: 'water', category: 'drinks', quantity: '4', measurement: 'cup/cups', recipe_id: brazilian_lemonade.id)
 
 
-ribeye_16oz = Ingredient.create(name: 'ribeye', category: 'meats', quantity: '16', measurement: 'ounces', shopping_list_id: monday_night_groceries.id)
+ribeye_16oz = Ingredient.create(name: 'ribeye', category: 'meats', quantity: '16', measurement: 'ounce/ounces', shopping_list_id: monday_night_groceries.id)
 
 salt_1_TBSP = Ingredient.create(name: 'salt', category: 'spices', quantity: '1', measurement: 'tablespoon', shopping_list_id: monday_night_groceries.id)
 
