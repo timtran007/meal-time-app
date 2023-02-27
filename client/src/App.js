0aa3dacdd39f4d194388970b402fb6a3f2fc5d54
Tim <timtran007@gmail.com>
2023-02-27 01:03:38 -0600

    builds out routes, set state with useEffect for recipes

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import { Route, Switch } from 'react-router-dom';
+import DetailedRecipeCard from './components/Cards/DetailedRecipeCard/DetailedRecipeCard';
+import LoginPage from './pages/LoginPage/LoginPage';
+import RecipePage from './pages/RecipePage/RecipePage';
+import SignupPage from './pages/SignupPage/SignupPage';
+
+
 
 function App() {
+
+  const [recipes, setRecipes] = useState([])
+  
+  useEffect(() => {
+    fetch('/recipes')
+    .then(resp => resp.json())
+    .then(data => setRecipes(data))
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Meal Time</h1>
+        
+        <Switch>
+          <Route exact path='/'>
+            <RecipePage recipes={recipes}/>
+          </Route>
+          <Route path='/recipes/:recipe_id'>
+            <DetailedRecipeCard recipes={recipes}/>
+          </Route>
+          
+
+          {/* <Route exact path='/profile'>
+            <UserPage />
+          </Route>
+          <Route exact path='/profile/recipes'>
+            <UserRecipesPage />
+          </Route>
+          <Route path='/profile/recipes/:recipe_id'>
+            <UserDetailedRecipesPage />
+          </Route>
+          <Route path='/profile/shopping-lists'>
+            <UserShoppingListsPage />
+          </Route>
+          <Route path='/login'>
+            <LoginPage />
+          </Route>
+          <Route path='/sign-up'>
+            <SignupPage/>
+          </Route> */}
+        </Switch>
+      
+        
       </header>
     </div>
   );
