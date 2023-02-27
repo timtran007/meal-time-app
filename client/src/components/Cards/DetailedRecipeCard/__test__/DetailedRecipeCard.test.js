4a0d37be6c05bccb53bc6c3b732d7b0791694ccb
Tim <timtran007@gmail.com>
2023-02-27 01:02:15 -0600

    updates the detailed recipe card test

--- client/src/components/Cards/DetailedRecipeCard/__test__/DetailedRecipeCard.test.js
+++ client/src/components/Cards/DetailedRecipeCard/__test__/DetailedRecipeCard.test.js
@@ -1,42 +1,58 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import DetailedRecipeCard from '../DetailedRecipeCard'
 
+const recipeProps =
+        [{
+            id: 2,
+            title: "Brazilian Lemonade",
+            instructions: "Step 1: cut off lime ends\nStep 2: cut into 8 wedges\nStep 3: combine limes into blender with water, condensed milk and sugar, \nStep 4: pulse 5 to 6 times,\nStep 5: strain out drink and discard pieces in the strainer\nStep 6: pour over ice and serve",
+            cook_time_in_minutes: 2,
+            prep_time_in_minutes: 8,
+            likes: 0,
+            user: {
+                id: 1,
+                name: "Tim",
+                email: "tim@gmail.com",
+                image_url: null
+            },
+            recipe_ingredients: [
+                {
+                id: 5,
+                name: "lime",
+                category: "produce",
+                quantity: "8",
+                measurement: "ounce/ounces"
+                },
+                {
+                id: 6,
+                name: "sugar",
+                category: "baking",
+                quantity: "1/3",
+                measurement: "cup/cups"
+                },
+                {
+                id: 7,
+                name: "water",
+                category: "drinks",
+                quantity: "4",
+                measurement: "cup/cups"
+                }
+                ]
+        }]
+function MockDetailedRecipeCard(){
+    return(
+        <BrowserRouter>
+            <DetailedRecipeCard recipes={recipeProps}/>
+        </BrowserRouter>
+    )
+}
 describe('DetailedRecipeCard', () => {
 
-    it('renders the title of the recipe', () => {
-        render(<DetailedRecipeCard />);
-        const title = screen.getByText(/title/i)
+    it('properly renders Detailed Recipe Card', async () => {
+        render(<MockDetailedRecipeCard />);
+        const title = await screen.findByText(/loading/i)
         expect(title).toBeInTheDocument()
-    })
-
-    it('renders recipe ingredients in a list', () => {
-        render(<DetailedRecipeCard />);
-        const lists = screen.getByRole('list')
-        expect(lists).toBeInTheDocument
-    })
-
-    it('renders the instructions of the recipe', () => {
-        render(<DetailedRecipeCard />);
-        const instructions = screen.getByText(/instructions/i)
-        expect(instructions).toBeInTheDocument()
-    })
-
-    it('renders the prepTime of the recipe', () => {
-        render(<DetailedRecipeCard />);
-        const prepTime = screen.getByText(/prep time/i)
-        expect(prepTime).toBeInTheDocument()
-    })
-
-    it('renders the cookTime of the recipe', () => {
-        render(<DetailedRecipeCard />);
-        const cookTime = screen.getByText(/cook time/i)
-        expect(cookTime).toBeInTheDocument()
-    })
-
-    it('renders the likes of the recipe', () => {
-        render(<DetailedRecipeCard />);
-        const likes = screen.getByText(/likes/i)
-        expect(likes).toBeInTheDocument()
     })
 })
 
