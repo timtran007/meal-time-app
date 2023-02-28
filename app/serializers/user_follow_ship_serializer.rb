f2b8e4ec15d6894fd79dd42351ee72d9b0305421
Tim <timtran007@gmail.com>
2023-02-28 00:37:09 -0600

    updates the user followship serializer

--- app/serializers/user_follow_ship_serializer.rb
+++ app/serializers/user_follow_ship_serializer.rb
@@ -1,5 +1,5 @@
 class UserFollowShipSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :image_url, :following, :followers, :shopping_lists
+  attributes :id, :name, :email, :image_url, :following, :followers, :shopping_lists, :recipes
 
   def following
     object.followings.map do |following| {
@@ -23,9 +23,27 @@ class UserFollowShipSerializer < ActiveModel::Serializer
   end
 
   def shopping_lists
-    object.shopping_lists
+    object.shopping_lists.map do |shopping_list| {
+      id: shopping_list.id,
+      name: shopping_list.name,
+      ingredients: shopping_list.ingredients,
+    }
+    end
   end
 
-  has_many :recipes
+  def recipes
+    object.recipes.map do |recipe| {
+      id: recipe.id,
+      title: recipe.title,
+      instructions: recipe.instructions,
+      image_url: recipe.image_url,
+      cook_time: recipe.cook_time_in_minutes,
+      prep_time: recipe.prep_time_in_minutes,
+      likes: recipe.likes,
+      recipe_ingredients: recipe.recipe_ingredients
+    }
+    end
+  end
+
  
 end
