12b1b1ec1e5921467708c83b7aa4d3cb14738742
Tim <timtran007@gmail.com>
2023-03-01 09:44:45 -0600

    adds a shopping list button to add a shopping list, ingredient form and delete list button

--- client/src/pages/UserShoppingListsPage/UserShoppingListsPage.js
+++ client/src/pages/UserShoppingListsPage/UserShoppingListsPage.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
 import ShoppingListCard from '../../components/Cards/ShoppingListCard/ShoppingListCard'
+import NewShoppingListIngredientForm from '../../components/Forms/NewShoppingListIngredientForm/NewShoppingListIngredientForm'
 
 function UserShoppingListsPage({user}) {
   return (
     <div>
         <h2>My Shopping Lists</h2>
+        <Button onClick={() => {}}>Add a Shopping List</Button>
         {user.shopping_lists.map(shopping_list => {
             return(
                 <div key={shopping_list.id}>
                     <ShoppingListCard shopping_list={shopping_list}/>
+                    <NewShoppingListIngredientForm />
+                    <Button onClick={() => {}}>delete list</Button>
                 </div>
             )
         })}
