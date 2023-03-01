662cd57ef100dcb0936140f3b014b852eb63a020
Tim <timtran007@gmail.com>
2023-02-28 20:39:49 -0600

    adds props to the test

--- client/src/pages/UserPage/__test__/UserPage.test.js
+++ client/src/pages/UserPage/__test__/UserPage.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import UserPage from '../UserPage'
 
 it('renders the User page successfully', () => {
-  render(<UserPage />);
+  render(<UserPage user={[]} followers={[]} following={[]}/>);
   const element = screen.getByText(/followers/i);
   expect(element).toBeInTheDocument();
 });
