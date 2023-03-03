1c4199bc8ca9ed0963950e2f01753c105463992e
Tim <timtran007@gmail.com>
2023-03-03 11:43:07 -0600

    updates the navigation test

--- client/src/components/Navigation/__test__/Navigation.test.js
+++ client/src/components/Navigation/__test__/Navigation.test.js
@@ -3,18 +3,6 @@ import Navigation from '../Navigation'
 
 const mockedSetUser = jest.fn()
 
-describe('Navigation when user is logged out', () => {
-    it('renders the login page successfully', () => {
-        render(
-            <Navigation 
-                user={[]}
-                setUser={mockedSetUser}
-            />);
-        const text = screen.getByText(/login/i)
-        expect(text).toBeInTheDocument();
-    });
-})
-
 
 describe('Navigation when user is logged in', () => {
     it('renders the login page successfully', () => {
