68503d32aaae6397f609ea6cd40a0dc96c789eac
Tim <timtran007@gmail.com>
2023-03-13 14:15:08 -0500

    ready for production

---
+++ bin/meal-time-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Add build commands for front end
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
+bundle install
+# bundle exec rake assets:precompile # These lines are commented out because we have an API only app
+# bundle exec rake assets:clean
+bundle exec rake db:migrate
+bundle exec rake db:seed
