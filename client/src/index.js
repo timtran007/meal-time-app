c2facc5e28aac1b1feb7d8c1d91fa5649a779c89
Tim <timtran007@gmail.com>
2023-03-12 22:23:48 -0500

    updates with SSRProvider

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SSRProvider from 'react-bootstrap/SSRProvider';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <SSRProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+  </SSRProvider>
   
 );
 
