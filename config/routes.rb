d35cf163e977c92df038a5f4688df5ef2d272f6e
Tim <timtran007@gmail.com>
2023-03-13 22:55:45 -0500

    updates for client side routing in Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,26 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :following_ships, only: [:create, :update, :destroy]
+    resources :recipes
+    resources :shopping_lists
+    resources :recipe_ingredients, only: [:create, :update, :destroy]
+    resources :ingredients, only: [:create, :update, :destroy]
 
-  resources :following_ships, only: [:create, :update, :destroy]
-  resources :recipes
-  resources :shopping_lists
-  resources :recipe_ingredients, only: [:create, :update, :destroy]
-  resources :ingredients, only: [:create, :update, :destroy]
 
+    get '/users', to: 'users#index'
+    get '/user-recipes', to: 'users#user_recipes'
+    post '/signup', to: 'users#create'
+    get '/me', to: 'users#show'
 
-  get '/users', to: 'users#index'
-  get '/user-recipes', to: 'users#user_recipes'
-  post '/signup', to: 'users#create'
-  get '/me', to: 'users#show'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
 
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+    get '/following-recipes', to: 'following_ships#show_following_recipes'
 
-  get '/following-recipes', to: 'following_ships#show_following_recipes'
+    post '/follow-user-recipes/', to: 'recipes#follow_user'
+  end
 
-  post '/follow-user-recipes/', to: 'recipes#follow_user'
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
   # # feature will be developed at a later time
   # patch 'recipe-likes/:id', to: 'recipes#update_likes'
