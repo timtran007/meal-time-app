82789ecc3b947c4e1068e21b6b79750e8421490f
Tim <timtran007@gmail.com>
2023-03-13 23:22:13 -0500

    updates routes to fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,17 @@
 Rails.application.routes.draw do
-  namespace :api do
     resources :following_ships, only: [:create, :update, :destroy]
     resources :recipes
     resources :shopping_lists
     resources :recipe_ingredients, only: [:create, :update, :destroy]
     resources :ingredients, only: [:create, :update, :destroy]
-
-
     get '/users', to: 'users#index'
     get '/user-recipes', to: 'users#user_recipes'
     post '/signup', to: 'users#create'
     get '/me', to: 'users#show'
-
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
-
     get '/following-recipes', to: 'following_ships#show_following_recipes'
-
     post '/follow-user-recipes/', to: 'recipes#follow_user'
-  end
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
